5b599babf73a71c90f82fb0bc06d74875f72e77d
himanshu <kumarhimanshu901@gmail.com>
2020-03-26 23:02:30 +0530

    Description added in test.py

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
+'''
+Part 1: Store the data in Google Spreadsheet.
+Part 2: Create a webservice using  Django which can handle below requests
+from the client side, using Google Sheets as the source. The request and response must be in JSON format.
+ '''
+
 import requests
 import json
 Base_Url="http://127.0.0.1:8000/"
+
+'''1. Show how many records that do not have "cellular" contact.'''
 
 def getcontact(contact):
     endpoint = 'BankContact/'
@@ -10,6 +18,9 @@ def getcontact(contact):
     data=json.dumps(data)
     response=requests.get(Base_Url+endpoint,data=data)
     print(json.dumps(response.json(),indent=4))
+#getcontact("cellular")
+
+'''2. Use "day" and "month" column values and find records which are after 15th October'''
 
 def getdates(day,month):
     endpoint = 'BankDayMonthFilter/'
@@ -21,6 +32,10 @@ def getdates(day,month):
     response=requests.get(Base_Url+endpoint,data=data)
     print(json.dumps(response.json(),indent=4))
 
+#getdates(15,"oct")
+
+'''3. How many records having a specific marital status and between certain age group.'''
+
 def getMarital(start_age,end_age,marital_status):
     endpoint = 'BankMarital/'
     data={
@@ -31,4 +46,5 @@ def getMarital(start_age,end_age,marital_status):
     data=json.dumps(data)
     response=requests.get(Base_Url+endpoint,data=data)
     print(json.dumps(response.json(),indent=4))
-getMarital(20,50,"married")
+
+#getMarital(20,50,"married")
